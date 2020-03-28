ec5db2006163773fb4620e031480612d38574919
Elias.lima <elias_dos_reis@hotmail.com>
2020-03-28 17:45:07 -0300

    Criando sistema de dano multiplayer

--- Assets/MyGame/Scripts/BulletController.cs
+++ Assets/MyGame/Scripts/BulletController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class BulletController : MonoBehaviour {
 
@@ -8,13 +9,19 @@ public class BulletController : MonoBehaviour {
 	public float speedBullet = 100f;
 	public Rigidbody2D rigid;
 
-	public float bulletTimeLife = 5f;
+	public float bulletTimeLife = 50f;
 	float bulletTimeCount;
+
+    public float bulletDamage;
+
+    private PhotonView photonview;
 
 	void Start () {
 		rigid = GetComponent<Rigidbody2D> ();
 
 		rigid.AddForce (transform.up * speedBullet, ForceMode2D.Force);
+
+        photonview = GetComponent<PhotonView>();
 	}
 	
 	// Update is called once per frame
@@ -25,4 +32,26 @@ public class BulletController : MonoBehaviour {
 
 		bulletTimeCount += Time.deltaTime;
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && other.GetComponent<PlayerController>() && other.GetComponent<PhotonView>().IsMine )
+        {
+            other.GetComponent<PlayerController>().TakeDamage(bulletDamage);
+            DestroyBullet();
+            Debug.Log("Player.:"+ other.GetComponent<PhotonView>().Owner.NickName   );
+        }
+    }
+    
+    void DestroyBullet()
+    {
+        photonview.RPC("DestroyBulletNetwork", RpcTarget.AllViaServer);
+    }
+
+    [PunRPC]
+    void DestroyBulletNetwork()
+    {
+        Destroy(gameObject);
+    }
+
 }
